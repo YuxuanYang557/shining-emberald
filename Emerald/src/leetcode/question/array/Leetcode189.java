eb8055f06fff9008cf6a13886e9832ff2f0b2a11
YuXuan Yang <yang.yux@husky.neu.edu>
2019-09-29 13:51:12 -0400

    working on array data structure exercise

---
+++ Emerald/src/leetcode/question/array/Leetcode189.java
@@ -0,0 +1,34 @@
+package leetcode.question.array;
+
+public class Leetcode189 {
+	
+	//method1 rotate the element in place
+	public void rotate(int[] nums, int k) {
+		rotateNums(nums, 0, nums.length -1);
+		rotateNums(nums, 0, k);
+		rotateNums(nums, k , nums.length-1);
+	}
+	
+	public void rotateNums(int[] nums, int start, int end) {
+		while(start < end) {
+			int temp = nums[start];
+			nums[start] = nums[end];
+			nums[end] = temp;
+			start ++;
+			end ++;
+		}
+	}
+	
+	//method2 generate another copy of array and get part of it;
+	public void rotate2(int[] nums, int k) {
+		k = k%nums.length;
+		int[] nums2 = new int[nums.length *2];
+		for(int i = 0; i< nums.length; i++) {
+			nums2[i] = nums[i];
+			nums2[i+nums.length] = nums[i];
+		}
+		for(int j = 0; j< nums.length; j++) {
+			nums[j] = nums2[nums.length -k +j];
+		}
+	}
+}
---
+++ Emerald/src/leetcode/question/array/Leetcode66.java
@@ -0,0 +1,20 @@
+package leetcode.question.array;
+
+public class Leetcode66 {
+
+	public int[] plusOne(int[] digits) {
+		int n = digits.length;
+		for(int i =0; i < n; i++) {
+			if(digits[i] < 9) {
+				digits[i]++;
+				return digits;
+			}else {
+				digits[i] = 0;
+			}
+		}
+		int[] newNum = new int[n+1];
+		newNum[0] = 1;
+		
+		return newNum;
+	}
+}
